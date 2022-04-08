5dbb26dd7cd361b4764a641f798b6354510169c1
reby22 <ramf2298@gmail.com>
2022-04-07 21:44:10 -0500

    nuevo Commit unit test

--- src/uaslp/objetos/list/arraylist/ArrayList.java
+++ src/uaslp/objetos/list/arraylist/ArrayList.java
@@ -2,99 +2,106 @@ package uaslp.objetos.list.arraylist;
 
 import uaslp.objetos.list.Iterator;
 import uaslp.objetos.list.List;
+import uaslp.objetos.list.exception.NotNullValuesAllowedException;
+import uaslp.objetos.list.exception.NotValidIndexException;
 
-public class ArrayList <T> implements List<T>{
-
-    private static final int DEFAULT_SIZE = 50;
+public class ArrayList<T> implements List<T> {
+    private  static final int DEFAULT_SIZE = 2;
     private T[] array;
     private int size;
 
-    public static String getName(){
-        return "ArrayList";
+    public ArrayList(int size){
+        array= (T[]) new Object[size];
     }
 
     public ArrayList(){
-        array = (T[])new Object[DEFAULT_SIZE];
-    }
-
-    public ArrayList(int size){
-        array = (T[])new Object[size];
+        array= (T[]) new Object[DEFAULT_SIZE];
     }
 
     @Override
-    public void addAtTail(T data) {
-        if(size == array.length){
-            increaseArraySize();
+    public void addAtTail(T data) throws NotNullValuesAllowedException{
+
+        if(data == null){
+            throw new NotNullValuesAllowedException();
         }
 
-        array[size] = data;
+        if(size == array.length){
+            increaseArrayList();
+        }
+
+        array[size]=data;
         size++;
     }
 
     @Override
-    public void addAtFront(T data) {
-        if (size >= 0){
-            System.arraycopy(array, 0, array, 1, size);
+    public void addAtFront(T data) throws NotNullValuesAllowedException{
+
+        if(data == null){
+            throw new NotNullValuesAllowedException();
         }
+
+        if(size == array.length){
+            increaseArrayList();
+        }
+        if (size >= 0) System.arraycopy(array, 0, array, 1, size);
         array[0]=data;
         size++;
     }
 
     @Override
-    public void remove(int index) {
-        if(index < 0 || index >= size){
+    public void remove(int index){
+        if (index < 0 || index >= size) {
             return;
         }
 
-        if (size - 1 - index >= 0){
-            System.arraycopy(array, index + 1, array, index, size - 1 - index);
-        }
-        array[size-1] = null;
+        if (size - 1 - index >= 0) System.arraycopy(array, index + 1, array, index, size - 1 - index);
+        array[size-1]=null;
         size--;
     }
 
     @Override
-    public void removeAll() {
-        for(int i=0;i<size;i++){
+    public void removeAll(){
+        for(int i=0;i<size;i++)
+        {
             array[i]=null;
         }
         size=0;
     }
 
     @Override
-    public void setAt(int index, T data) {
-        if(index >=0 && index < size){
-            array[index] = data;
+    public T getAt(int index){
+        return index >= 0 && index < size ? array[index] : null;
+    }
+
+    @Override
+    public void setAt(int index,T data) throws NotNullValuesAllowedException {
+
+        if(data == null){
+            throw new NotNullValuesAllowedException();
+        }
+
+        if(index >= 0 && index < size)
+        {
+            array[index]=data;
         }
     }
 
-    /**
-     * @param index 0-index
-     * @return element at position index
-     */
-
     @Override
-    public T getAt(int index) {
-        return index >= 0 && index < size  ? array[index] : null;
-    }
-
-    @Override
-    public Iterator<T> getIterator() {
+    public Iterator<T> getIterator(){
         return new ArrayListIterator<>(this);
     }
-
     @Override
-    public int getSize() {
+    public int getSize(){
         return size;
     }
 
-    private void increaseArraySize(){
+    private void increaseArrayList(){
         T []newArray = (T[])new Object[array.length * 2];
 
         for(int i=0;i<size;i++){
             newArray[i]=array[i];
         }
 
-        array = newArray;
+        array=newArray;
     }
 }
